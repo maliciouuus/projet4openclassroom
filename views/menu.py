cb3d8397138ecff7033987e5ba0244c3aae5bb30
VotreNouveauNom <votre.nouvel.email@example.com>
2025-01-31 00:50:39 +0000

    menu en cours

--- views/menu.py
+++ views/menu.py
@@ -4,5 +4,14 @@
 
 class MainView(View):
 
-    def montrermenu(self):
-        print("cc")
+    def afficher_menu():
+        print("Centre Échecs")
+        print("--------------------")
+        print("1. Ajouter un joueur")
+        print("2. Afficher la liste des joueurs")
+        print("3. Créer un tournoi")
+        print("4. Afficher la liste des tournois")
+        print("5. Afficher les rapports")
+        print("6. Quitter")
+        print("--------------------")
+        number_menu = int(input("Veuillez saisir le chiffre selon l'option soujaité:"))
